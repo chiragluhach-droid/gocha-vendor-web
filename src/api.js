04fc47e486a961ef43f77865519ee2d553e9b58c
chirag-credence <luhachchirag@gmail.com>
2026-01-31 09:58:06 +0530

    Switch backend to Cloudflare HTTPS

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://ware-mental-progressive-sensors.trycloudflare.com';
+const API_BASE_URL = 'https://gocha-backend.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
